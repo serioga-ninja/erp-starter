c51e04288e086376ff8eefe215631a0737c733b6
SDatsii <sdatsiy@morebis.net>
2025-02-25 15:02:43 +0200

    Add global exception filter

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { ProjectConfig } from '@app/common';
+import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { AllExceptionsFilter } from '@app/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new AllExceptionsFilter());
   const config = app.get(ConfigService<ProjectConfig, true>);
 
   await app.listen(config.get('PORT'));
